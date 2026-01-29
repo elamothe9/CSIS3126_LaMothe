24e8ae8fe83a19aae66595daa54d2e7a5bc4e691
elamothe9 <evanlamothe9@gmail.com>
2026-01-29 12:56:56 -0500

    Added stats page, game page, set admin page and intents/buttons to splash page

---
+++ app/src/main/java/com/example/myapplication/MainMenuActivity.kt
@@ -0,0 +1,45 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.util.Log
+
+class MainMenuActivity : AppCompatActivity(){
+
+    private lateinit var btnViewStats: Button
+    private lateinit var btnStartGame: Button
+    private lateinit var btnSetAdmin: Button
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main_menu)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        this.btnSetAdmin = findViewById(R.id.btnSetAdmin)
+        this.btnViewStats = findViewById(R.id.btnViewStats)
+        this.btnStartGame = findViewById(R.id.btnStartGame)
+        //On click listeners, sending user to different activities based on which one they press
+        this.btnStartGame.setOnClickListener {
+            val startGameIntent = Intent(this, GameActivity::class.java)
+            startActivity(startGameIntent)
+    }
+        this.btnViewStats.setOnClickListener {
+            val viewStatsIntent = Intent(this, StatisticsActivity::class.java)
+            startActivity(viewStatsIntent)
+        }
+        this.btnSetAdmin.setOnClickListener {
+            val setAdminIntent = Intent(this, SetAdminActivity::class.java)
+            startActivity(setAdminIntent)
+        }
+    }
+}
